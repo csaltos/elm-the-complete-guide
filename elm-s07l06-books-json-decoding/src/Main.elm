8ad2d10eff7b6081eeedc6a4540aae03e79e8e67
Carlos Saltos <csaltos@talenteca.io>
2022-03-05 23:52:13 +0200

    Adding Books JSON decoding

---
+++ elm-s07l06-books-json-decoding/src/Main.elm
@@ -0,0 +1,155 @@
+module Main exposing (..)
+
+import Browser
+import Element as E
+import Element.Background as EBG
+import Element.Border as EB
+import Element.Font as EF
+import Element.Input as EI
+import Html
+import Http
+import Json.Decode as JD
+
+
+type alias Model =
+    { searchText : String
+    , results : List Book
+    }
+
+type alias Book =
+    { title : String
+    , thumbnail : String
+    , link: String
+    }
+
+type Msg
+    = MsgSearch
+    | MsgGotResults (Result Http.Error (List Book))
+    | MsgInputTextField String
+
+
+main : Program () Model Msg
+main =
+    Browser.element
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
+
+
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( initModel, Cmd.none )
+
+
+initModel : Model
+initModel =
+    { searchText = ""
+    , results = []
+    }
+
+
+view : Model -> Html.Html Msg
+view model =
+    viewLayout model
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        MsgInputTextField newTextInput ->
+            ( { model | searchText = newTextInput }, Cmd.none )
+
+        MsgSearch ->
+            ( model, cmdSearch model)
+
+        MsgGotResults result ->
+            case result of
+                Ok data ->
+                    ( { model | results = data }, Cmd.none )
+                Err error ->
+                    ( model, Cmd.none )
+
+
+subscriptions : model -> Sub msg
+subscriptions _ =
+    Sub.none
+
+
+viewLayout : Model -> Html.Html Msg
+viewLayout model =
+    E.layoutWith
+        { options =
+            [ E.focusStyle
+                { borderColor = Nothing
+                , backgroundColor = Nothing
+                , shadow = Nothing
+                }
+            ]
+        }
+        []
+        (E.column [] [ viewSearchBar model, viewResults model])
+
+
+viewSearchBar : Model -> E.Element Msg
+viewSearchBar model =
+    E.row []
+        [ EI.search []
+            { onChange = MsgInputTextField
+            , text = model.searchText
+            , placeholder = Nothing
+            , label = EI.labelLeft [] (E.text "Search books:")
+            }
+        , viewSearchButton
+        ]
+
+viewResults : Model -> E.Element msg
+viewResults model =
+    E.column [] [
+        case List.head model.results of
+            Just firstItem ->
+                E.text firstItem.title
+            Nothing ->
+                E.text "Empty"
+    ]
+
+viewSearchButton : E.Element Msg
+viewSearchButton =
+    EI.button
+        [ EBG.color (E.rgb255 0x00 0x33 0x66)
+        , EF.color (E.rgb255 0xEE 0xEE 0xEE)
+        , EB.rounded 5
+        , E.padding 12
+        , E.mouseOver
+            [ EBG.color (E.rgb255 0x33 0x66 0x99)
+            , EF.color (E.rgb255 0xDD 0xDD 0xDD)
+            ]
+        ]
+        { onPress = Just MsgSearch
+        , label = E.text "Search"
+        }
+
+cmdSearch model =
+    Http.get {
+        url = "https://www.googleapis.com/books/v1/volumes?q=" ++ model.searchText
+        , expect = Http.expectJson MsgGotResults decodeJson
+    }
+
+decodeJson =
+    JD.field "items" decodeItems
+
+decodeItems =
+    JD.list decodeItem
+
+decodeItem =
+    JD.field "volumeInfo" decodeVolumeInfo
+
+decodeVolumeInfo =
+    JD.map3 Book
+        (JD.field "title" JD.string)
+        (JD.field "imageLinks" decodeImageLinks)
+        (JD.field "canonicalVolumeLink" JD.string)
+
+decodeImageLinks =
+    JD.field "thumbnail" JD.string

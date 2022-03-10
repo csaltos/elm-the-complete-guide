6068a1c2f2a2a0cf424e5571d11af9e6f963ef6c
Carlos Saltos <csaltos@talenteca.io>
2022-03-10 17:21:37 +0200

    Adding Mini Blog subscriptions

---
+++ elm-s11l11-mini-blog-subscriptions/src/HomePage.elm
@@ -0,0 +1,43 @@
+module HomePage exposing (..)
+
+import Element
+import Element.Font
+
+
+type Msg
+    = MsgDummy
+
+
+type alias Model =
+    { counter : Int
+    }
+
+
+initModel : Model
+initModel =
+    { counter = 0
+    }
+
+
+view : Model -> Element.Element msg
+view model =
+    Element.column [ Element.padding 20 ]
+        [ Element.text ("My blog " ++ String.fromInt model.counter)
+        , Element.paragraph
+            [ Element.paddingXY 0 20
+            , Element.Font.size 14
+            ]
+            [ Element.text "Welcome to my mini blog made with Elm and Elm UI"
+            ]
+        ]
+
+
+update : Msg -> Model -> (Model, Cmd.Cmd Msg)
+update msg model =
+    case msg of
+        MsgDummy ->
+            (model, Cmd.none)
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
